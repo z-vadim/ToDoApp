05778b233ab69a4d73fd8a0a995dcf728626d221
vadim <z_vadim@outlook.com>
2015-02-26 18:27:03 +0200

    small decorative changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 //===== db =====
 module.exports = {
-	url: 'mongodb://admin:admin@ds049161.mongolab.com:49161/todo';
+	url: 'mongodb://admin:admin@ds049161.mongolab.com:49161/todo'
 };
--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-
-//===== db =====
-mongoose.connect('mongodb://admin:admin@ds049161.mongolab.com:49161/todo');
+//----- db -----
+var database = require('./config/database');
+mongoose.connect(database.url);
 
 //----- config -----
 app.use(express.static(__dirname + '/public'));
@@ -16,53 +16,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-var Todo = mongoose.model('Todo', {
-	text: String
-});
 
 //===== routes =====
-//----- show all todos -----
-app.get('/api/todos', function(req, res){
-	Todo.find(function(err, todos){
-		if(err)
-			res.send(err);
-		res.json(todos);
-	});
-});
-//----- create todo -----
-app.post('/api/todos', function(req, res){
-	Todo.create({
-		text: req.body.text,
-		done: false
-	}, function(err, todo){
-		if (err)
-			res.send(err);
-		Todo.find(function(err, todos){
-			if (err)
-				res.send(err);
-			res.json(todos);
-		});
-	});
-});
-//----- delete todo -----
-app.delete('/api/todos/:todo_id', function(req, res){
-	Todo.remove({
-		_id : req.params.todo_id
-	}, function(err, todo){
-		if (err)
-			res.send(err);
-		Todo.find(function(err, todos){
-			if (err)
-				res.send(err);
-			res.json(todos);
-		});
-	});
-});
-//----- frontend -----
-app.get('*', function(req, res){
-	res.sendfile('./public/index.html');
-});
-
+require('./app/routes')(app);
 //===== listen =====
 app.listen(3000);
 console.log("App listening on port 3000");
